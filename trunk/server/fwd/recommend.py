2b61de03042f889f7ae9adb93a8b4c39e9979e1a
esperteria <esperteria@64f826ee-31e5-11de-94d6-134fa40061eb>
2009-04-14 02:25:48 +0000

    integrated NLTK to manage tokenization, stemming and frequency distribution counts. pushing this information successfully to the django server

--- trunk/server/fwd/recommend.py
+++ trunk/server/fwd/recommend.py
@@ -3,6 +3,7 @@ from django.core import serializers
 from django.contrib.auth.models import User 
 from models import *
 from django.contrib.auth.decorators import login_required
+import nltk
 
 @login_required
 def recommend(request):
@@ -24,6 +25,7 @@ def recommend(request):
   except Post.DoesNotExist:
     post = Post(url=post_url, feed=feed, title=post_title, contents=post_contents)
     post.save()
+    create_term_vector(post)    
   try:
     sharer = Sharer.objects.get(user=sharer_user)
   except Sharer.DoesNotExist:
@@ -42,9 +44,25 @@ def recommend(request):
   except SharedPost.DoesNotExist:
     shared_users = []
   
+
   json = serializers.serialize('json', users)
   shared_json = serializers.serialize('json', shared_users)
   
   script_output = '{posturl: \'' + post.url + '\', users: ' + json + ', shared: ' + shared_json + '}'
   return HttpResponse(script_output, mimetype='application/json')
 
+def create_term_vector(post):
+  "Creates objects for the term vector for the post"
+  frequency_distribution = post.tokenize()
+  for frequency_item in frequency_distribution.items():
+    try:
+      term = Term.objects.get(term=frequency_item[0])
+    except Term.DoesNotExist:
+      term = Term(term=frequency_item[0])
+      term.save()
+
+    term_vector_cell = TermVectorCell(term=term, count=frequency_item[1],
+                                      post=post)
+    term_vector_cell.save()
+        
+        
