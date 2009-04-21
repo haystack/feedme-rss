2aac9d8d9abebe5120441b0fb6f25413f4d85ac7
esperteria <esperteria@64f826ee-31e5-11de-94d6-134fa40061eb>
2009-04-21 00:18:52 +0000

    refactored share endpoint

--- trunk/server/fwd/share.py
+++ trunk/server/fwd/share.py
@@ -4,35 +4,30 @@ from django.contrib.auth.models import User
 from models import *
 from django.contrib.auth.decorators import login_required
 import re
+from django.core.mail import EmailMultiAlternatives
+import nltk
 
 @login_required
-def share(request, toggle, recipient_username=None, recipient_email=None):
+def share(request):
   post_url = request.POST['post_url']
-  share = int(toggle) == 1 # true if we're sharing, false if we're canceling
+  recipient_emails = request.POST.getlist('recipients')
+  comment = request.POST['comment']
 
-  # get the individuals being referred to
-  user_sharer = request.user
-  # get the recipient, creating a User if necessary
-  if recipient_email != None:
-    user_matches = User.objects.filter(email=recipient_email)
-    if len(user_matches) == 0:
-      # create a new user and specify they are recipients only
-      user_receiver = User.objects.create_user('fwd_' + format_email(recipient_email), recipient_email)
-    else:
-      user_receiver = user_matches[0]
-  else:
-    user_receiver = User.objects.get(username=recipient_username)
-  
+  shared_post = create_shared_post(request.user, \
+                                   post_url, recipient_emails, comment)
+  send_post(shared_post)
+
+  script_output = "{\"response\": \"ok\"}"
+  return HttpResponse(script_output, mimetype='application/json')
+
+def create_shared_post(user_sharer, post_url, recipient_emails, comment):
+  """Create all necessary objects to perform the sharing action"""
+  # get the recipients, creating Users if necessary
   try:
     sharer = Sharer.objects.get(user=user_sharer)
   except Sharer.DoesNotExist:
     sharer = Sharer(user=user_sharer)
     sharer.save()
-  try:
-    receiver = Receiver.objects.get(user=user_receiver)
-  except Receiver.DoesNotExist:
-    receiver = Receiver(user=user_receiver)
-    receiver.save()
 
   # get the post
   post = Post.objects.get(url=post_url)
@@ -40,26 +35,77 @@ def share(request, toggle, recipient_username=None, recipient_email=None):
     shared_post = SharedPost.objects.get(post=post, sharer=sharer)
   except SharedPost.DoesNotExist:
     shared_post = SharedPost(post=post, sharer=sharer)
-    shared_post.save()
-  try:
-    # protection against somehow suggesting it twice
-    shared_post_receiver = SharedPostReceiver.objects.get(shared_post=shared_post, receiver=receiver)
-  except SharedPostReceiver.DoesNotExist:
-    shared_post_receiver = SharedPostReceiver(shared_post=shared_post, receiver=receiver)
-    shared_post_receiver.save()
+  shared_post.comment = comment
+  shared_post.save()
 
-  #if share:
-    # e-mail will get sent automatically by cron process in ../send_mail.py
-    # send_post_email(shared_post, receiver)
-  if not share:
-    shared_post_receiver.delete()
-    if len(SharedPostReceiver.objects.filter(shared_post=shared_post)) == 0:
-      shared_post.delete()
-    
-  script_output = "{\"response\": \"ok\"}"
-  return HttpResponse(script_output, mimetype='application/json')
+  # get or create the recipients' User, Recipient and SharedPostRecipient
+  # objects
+  for recipient_email in recipient_emails:
+    try:
+      user_receiver = User.objects.get(email = recipient_email)
+    except User.DoesNotExist:
+      # Create a user with that username, email and password.
+      user_receiver = User.objects.create_user(recipient_email, \
+                                               recipient_email, \
+                                               recipient_email)
+      user_receiver.save()
+    try:
+      receiver = Receiver.objects.get(user=user_receiver)
+    except Receiver.DoesNotExist:
+      receiver = Receiver(user=user_receiver)
+      receiver.save()      
+    try:
+      shared_post_receiver = SharedPostReceiver.objects.get( \
+        shared_post=shared_post, receiver=receiver)
+    except SharedPostReceiver.DoesNotExist:
+      shared_post_receiver = SharedPostReceiver( \
+        shared_post=shared_post, receiver=receiver)
+      shared_post_receiver.save()
 
-# returns an email address reformatted to be a username (alphanumeric only)
-def format_email(email):
-  return re.sub(r'\W', '_', email)
+    return shared_post
 
+
+def send_post(post_to_send):
+    receivers = SharedPostReceiver.objects \
+                .filter(shared_post = post_to_send) \
+                .filter(sent__exact = False)
+
+    send_post_email(post_to_send, receivers)
+
+    receivers.sent = True
+    for receiver in receivers:
+        receiver.save()
+
+
+def send_post_email(shared_post, receivers):
+  "Sends the post in an email to the recipient"
+  post = shared_post.post
+  subject = post.title
+  from_email = shared_post.sharer.user.email
+  print receivers
+  to_emails = [receiver.receiver.user.email for receiver in receivers]
+  comment = shared_post.comment
+
+  print('sending ' + shared_post.post.title + \
+        ' to ' + str(to_emails) + '\n')
+  
+  html_content = u''
+  if comment is not u'':
+      html_content += comment + '<br /><br />'
+  html_content += shared_post.sharer.user.email + u" thought you might " + \
+                 u"like this post. " +\
+                 u"They're beta testing FeedMe, a tool we're developing " +\
+                 u"at MIT, so please feel free to <a href='mailto:feedme@" +\
+                 u"csail.mit.edu'>email us</a> with comments</a>." +\
+                 u"<br />\n<br />\n "
+  html_content += u"<b><a href='" + post.url + \
+                  u"'>" + post.title + u"</a></b> \n<br />"
+  html_content += post.contents
+  html_content += u"<br /><br /><span style='color: gray'>Sent via FeedMe: " +\
+                  u"a (very) alpha tool at MIT. Have comments, or are your " +\
+                  u"friends spamming you? Email us at feedme@csail.mit.edu."
+
+  text_content = nltk.clean_html(html_content)
+  email = EmailMultiAlternatives(subject, text_content, from_email, to_emails)
+  email.attach_alternative(html_content, "text/html")
+  email.send()
---
+++ trunk/server/send.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from models import *
+from django.contrib.auth.decorators import login_required
+import datetime
+from server.fwd.models import *
+
+def start_email_daemon():
+    """Creates a daemon to send emails every 20 seconds.  Uses daemon.py
+    to fork the process.  Warning: under default settings, this won't write
+    error messages to disk, so may just crash.  I think there's a way to
+    make it write errors somewhere.
+
+    Source: http://code.activestate.com/recipes/278731/
+    """
+    from django.core.management import setup_environ
+    import settings
+    setup_environ(settings)
+    import daemon
+
+    daemon.createDaemon()
+
+    debug_to_file = True
+    while True:
+        unsent = SharedPost.objects \
+                 .filter(sharedpostreceiver__sent__exact = False)
+        for shared_post in unsent:
+            send_post_email(shared_post)
+        time.sleep(20)
+
+
+
