1926a67cd41ba09699106ec5508f885a6fd20714
marcua <marcua@64f826ee-31e5-11de-94d6-134fa40061eb>
2009-06-18 20:33:28 +0000

    fix weird newline in subject bug

--- server/feedme/bookmarklet.py
+++ server/feedme/bookmarklet.py
@@ -1,14 +1,21 @@
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from server.feedme import recommend
 import nltk
 import re
+import html5lib
+from html5lib import treebuilders
+from BeautifulSoup import BeautifulSoup
 
 @login_required
 def bookmarklet(request):
-    recommend_output = recommend.get_recommendation_json(request)
     text = request.POST['post_contents']
+    parser = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder("beautifulsoup"))
+    text = parser.parse(text).prettify()
+
+    new_request = request.POST.copy() # because the request dict is immutable
+    new_request['post_contents'] = text
+    
     text = re.sub(r'class\s?=\s?".*?"', '', text)
     text = re.sub(r"class\s?=\s?'.*?'", '', text)
     text = re.sub(r'id\s?=\s?".*?"', '', text)
@@ -16,31 +23,18 @@ def bookmarklet(request):
     text = re.sub('style\s?=\s?".*?"', '', text)
     text = re.sub("style\s?=\s?'.*?'", '', text)    
     
-
     output = u'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">\n'
-#    output = u'<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\n'
-    output += u"<html><head>\n"
-    output += u"<script> var recommendations = " + unicode(recommend_output) + u";\n"
+    output += u"<html><head><script>\n"
     output += u"""
     var unsafeWindow = window;
-    function GM_addStyle(style) {
-    var styleSheet = null;
-    for (var i=0; i<document.styleSheets.length; i++) {
-         if (document.styleSheets[i].title == 'FeedMe') {
-              styleSheet = document.styleSheets[i];
-              break;
-         }
-    }
-    
-    if (styleSheet == null) {
-         var styleSheetElement = document.createElement('style');
-         styleSheetElement.type = 'text/css';
-         styleSheetElement.title = 'FeedMe';
-         // add to <head>
-         document.getElementsByTagName('head')[0].appendChild(styleSheetElement);
-    }
-    styleSheet = document.styleSheets[document.styleSheets.length - 1];
-    styleSheet.insertRule(style, 0);
+    function GM_addStyle(css) {
+      var head, style;
+      head = document.getElementsByTagName('head')[0];
+      if (!head) { return; }
+      style = document.createElement('style');
+      style.type = 'text/css';
+      style.innerHTML = css;
+      head.appendChild(style);
     }
 
     function GM_xmlhttpRequest(params) {
@@ -48,13 +42,22 @@ def bookmarklet(request):
               console.log('cannot do cross-domain request! canceling.');
               return;
          }
-         $.post(params.url, params.data, params.onload);
+         $.ajax({
+           type: "POST",
+           url: params.url,
+           data: params.data,
+           complete: function (request, status) {
+             params.onload(request);
+           }
+         });
+         //$.post(params.url, params.data, params.onload);
     }
     
     </script>
     """
-    output += u"<script type='text/javascript' src='http://feedme-rss.googlecode.com/svn/trunk/client/feedme.user.js'></script>"
+#    output += u"<script type='text/javascript' src='http://feedme-rss.googlecode.com/svn/trunk/client/feedme.user.js'></script>"
 #    output += u"<script type='text/javascript' src='http://people.csail.mit.edu/msbernst/dev/feedme.user.js'></script>"
+    output += u"<script type='text/javascript' src='http://people.csail.mit.edu/marcua/dev/feedme.user.js'></script>"
     output += u"<link type='text/css' href='http://groups.csail.mit.edu/haystack/feedme/greader.css' rel='stylesheet' />"
     output += u"""
     <!-- These are our tweaks to the GReader CSS to make things lay out correctly -->
@@ -106,13 +109,41 @@ def bookmarklet(request):
     </style>
     """
     output += u"</head><body class='gecko loaded'>"
-    output += u"<div id='main'><div id='chrome'><table id='chrome-viewer-container'><tbody><tr><td id='chrome-viewer'><div id='viewer-container'><div id='entries' class='list' style='visbility: visible;'><div id='current-entry' class='entry expanded read'><div class='entry-container'><div class='entry-main'><h2 class='entry-title'><a class='entry-title-link' href='" + request.POST['post_url'] + u"' target='_blank'>" + request.POST['post_title'] + u"</a></div><div class='entry-author'><span class='entry-source-title-parent'>from <a class='entry-source-title' target='_blank' href='" + request.POST['feed_url'] + u"'>" + request.POST['feed_title'] + u"</a></span><span style='display: none;'> by <span class='entry-author-name'></span></span></div><div class='entry-annotations'></div><div class='entry-body'><div><div class='item-body'>" + text + u"</div></div></div></div></div></div></div></div></td></tr></tbody></table></div></div>"
+    output += u"""
+    <div id='main'><div id='chrome'><table id='chrome-viewer-container'><tbody><tr><td id='chrome-viewer'><div id='viewer-container'><div id='entries' class='list' style='visbility: visible;'><div id='current-entry' class='entry expanded read'>
+   <div class='entry-container'>
+    <div class='entry-main'>
+      <h2 class='entry-title'>
+        <a class='entry-title-link' href='""" + request.POST['post_url'] + u"' target='_blank'>" + request.POST['post_title'] + u"""</a>
+      </h2>
+      <div class='entry-author'>
+        <span class='entry-source-title-parent'>from
+          <a class='entry-source-title' target='_blank' href='""" + request.POST['feed_url'] + u"'>" + request.POST['feed_title'] + u"""</a>
+        </span>
+        <span style='display: none;'> by 
+          <span class='entry-author-name'>
+          </span>
+          </span>
+      </div>
+      <div class='entry-annotations'>
+      </div>
+      <div class='entry-body'>
+        <div>
+          <div class='item-body'>"""
+    output += text.decode('utf-8')
+    output += u"""</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    </div></div></div></div></td></tr></tbody></table></div></div>
+    """
     output += u"""
     <script>
     $(document).ready(function() {
          autocompleteData = new Array();
          suggest_people($('#current-entry'));
-         populateSuggestions(recommendations);
     });
     </script>
     """
