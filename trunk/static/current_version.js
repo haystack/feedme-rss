bf8b81b057324e5fe1ca42969507eaa20ca0ff42
esperteria <esperteria@64f826ee-31e5-11de-94d6-134fa40061eb>
2009-06-28 01:46:17 +0000

    adding static files to repository

---
+++ trunk/static/current_version.js
@@ -0,0 +1,5 @@
+versionData = {
+	'version': 0.1,
+	'url': 'http://feedme-rss.googlecode.com/svn-history/r113/trunk/client/feedme.user.js',
+	'whats-new': 'This version introduces automatic update functionality.'
+}
