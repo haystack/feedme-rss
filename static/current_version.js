8efacb6e980878870568701f01b1a7c5709a0903
esperteria <esperteria@64f826ee-31e5-11de-94d6-134fa40061eb>
2009-07-05 02:57:41 +0000

    version 0.11, first public release of autoupdate

--- static/current_version.js
+++ static/current_version.js
@@ -1,5 +1,5 @@
 versionData = {
-	'version': 0.1,
-	'url': 'http://feedme-rss.googlecode.com/svn-history/r113/trunk/client/feedme.user.js',
-	'whats-new': 'This version introduces automatic update functionality.'
+	'version': 0.11,
+	'url': 'http://feedme-rss.googlecode.com/svn-history/r131/trunk/client/feedme.user.js',
+	'whats-new': 'This version introduces automatic update functionality and fixes several bugs with the new client UI.'
 }
