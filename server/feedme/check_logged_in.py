195d471c31ebc37edc0284add3c78be17094d4a3
esperteria <esperteria@64f826ee-31e5-11de-94d6-134fa40061eb>
2009-08-11 05:32:51 +0000

    debugging; trying to get contacts working in bookmarklet

--- server/feedme/check_logged_in.py
+++ server/feedme/check_logged_in.py
@@ -9,20 +9,23 @@ def check_logged_in(request):
     response['user_interface'] = True
     response['social_features'] = True
 
-    try:
-        sharer = Sharer.objects.get(user = request.user)
-        study_participant = StudyParticipant.objects.get(sharer = sharer)
-        response['user_interface'] = study_participant.user_interface
-        response['social_features'] = study_participant.social_features
-    except StudyParticipant.DoesNotExist:
-        print 'not a study participant'
-    
     response_json = simplejson.dumps(response)
 
     if response['logged_in']:
         # logging
         sharer = Sharer.objects.get(user = request.user)
         log = LoggedIn(sharer = sharer)
-        log.save()        
+        log.save()
+
+        # personalized UI
+        try:
+            sharer = Sharer.objects.get(user = request.user)
+            study_participant = StudyParticipant.objects.get(sharer = sharer)
+            response['user_interface'] = study_participant.user_interface
+            response['social_features'] = study_participant.social_features
+        except StudyParticipant.DoesNotExist:
+            print 'not a study participant'
+    
+        
 
     return HttpResponse(response_json, mimetype='application/json')
