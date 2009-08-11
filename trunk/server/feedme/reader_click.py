d4ad81c6beebff995108db29856f6d8a8c29c802
esperteria <esperteria@64f826ee-31e5-11de-94d6-134fa40061eb>
2009-08-11 05:32:51 +0000

    debugging; trying to get contacts working in bookmarklet

--- trunk/server/feedme/reader_click.py
+++ trunk/server/feedme/reader_click.py
@@ -14,12 +14,16 @@ def reader_click(request):
     feed_url = request.POST['feed_url']
     post_url = request.POST['post_url']
     sharer = Sharer.objects.get(user = request.user)
-    viewed_post = ViewedPost.objects \
+    viewed_posts = ViewedPost.objects \
                   .filter(post__feed__rss_url = feed_url) \
                   .filter(post__url=post_url) \
-                  .order_by('-time')[0]
-    viewed_post.link_clickthrough = True
-    viewed_post.save()
+                  .order_by('-time')
+    if len(viewed_posts) > 0:
+        viewed_post = viewed_posts[0]
+        viewed_post.link_clickthrough = True
+        viewed_post.save()
+    # TODO: this won't work if the recommend transaction hasn't committed
+    # and created the ViewedPost by the time this function is called
 
     script_output = "{\"response\": \"ok\"}"
     return HttpResponse(script_output, mimetype='application/json')
