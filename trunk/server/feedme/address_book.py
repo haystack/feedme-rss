652913a44de643087c069af8924d8f5bf27c8aeb
marcua <marcua@64f826ee-31e5-11de-94d6-134fa40061eb>
2009-08-13 21:50:28 +0000

    Fix that doesn't break client when user is not logged in

--- trunk/server/feedme/address_book.py
+++ trunk/server/feedme/address_book.py
@@ -4,13 +4,14 @@ from django.contrib.auth.models import User
 from models import *
 from django.contrib.auth.decorators import login_required
 
-@login_required
 def address_book(request):
+   if request.user.is_authenticated:
+       receivers = Receiver.objects.filter(
+           sharedpostreceiver__shared_post__sharer__user = request.user) \
+           .distinct()
+       response = [ { 'email': receiver.user.email } for receiver in receivers ]
+   else:
+       response = []
+   response_json = simplejson.dumps(response)
 
-    receivers = Receiver.objects.filter(
-        sharedpostreceiver__shared_post__sharer__user = request.user) \
-        .distinct()
-    response = [ { 'email': receiver.user.email } for receiver in receivers ]
-    response_json = simplejson.dumps(response)
-
-    return HttpResponse(response_json, mimetype='application/json')
+   return HttpResponse(response_json, mimetype='application/json')
