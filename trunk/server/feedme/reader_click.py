af89a4a51925a2771765140606239670485666e1
esperteria <esperteria@64f826ee-31e5-11de-94d6-134fa40061eb>
2009-08-16 20:52:30 +0000

    attempt to fix multithreaded reader_click bug

--- trunk/server/feedme/reader_click.py
+++ trunk/server/feedme/reader_click.py
@@ -9,22 +9,35 @@ from django.contrib.auth.decorators import login_required
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
 @login_required
+@transaction.commit_manually
 def reader_click(request):
     """Logs that a GReader user clicked through on a link in a post"""
     feed_url = request.POST['feed_url']
     post_url = request.POST['post_url']
-    sharer = Sharer.objects.get(user = request.user)
-    viewed_posts = ViewedPost.objects \
-                  .filter(post__feed__rss_url = feed_url) \
-                  .filter(post__url=post_url) \
-                  .order_by('-time')
-    if len(viewed_posts) > 0:
-        viewed_post = viewed_posts[0]
-        viewed_post.link_clickthrough = True
-        viewed_post.save()
-    # TODO: this won't work if the recommend transaction hasn't committed
-    # and created the ViewedPost by the time this function is called
 
+    # If the /recommend/ call hasn't returned yet, the objects won't be
+    # available to this transaction.  We keep looping until they are
+    keep_looping = True
+    while keep_looping:
+        try:
+            sharer = Sharer.objects.get(user = request.user)
+            viewed_posts = ViewedPost.objects \
+                           .filter(post__feed__rss_url = feed_url) \
+                           .filter(post__url=post_url) \
+                           .filter(sharer = sharer) \
+                           .order_by('-time')
+            if viewed_posts.count() == 0:
+                transaction.rollback()
+            else:
+                keep_looping = False
+        except Sharer.DoesNotExist:
+            transaction.rollback()
+    
+    viewed_post = viewed_posts[0]
+    viewed_post.link_clickthrough = True
+    viewed_post.save()
+
+    transaction.commit()
     script_output = "{\"response\": \"ok\"}"
     return HttpResponse(script_output, mimetype='application/json')
     
