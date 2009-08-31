a2a4649e3b37d93f132dda58744ee8983c2f5c91
esperteria <esperteria@64f826ee-31e5-11de-94d6-134fa40061eb>
2009-08-31 03:07:06 +0000

    draft of splitting sharedposts script. can be safely run with no side effects right now, will just output to the console any changes that should be made

---
+++ server/split_sharedposts.py
@@ -0,0 +1,73 @@
+from django.core.management import setup_environ
+import settings
+setup_environ(settings)
+from server.feedme.models import *
+from datetime import datetime, timedelta
+
+split_delta = timedelta(seconds = 5)
+def split_sharedposts():
+    """Finds and splits all sharedposts with multiple receivers over a large period of time
+    into separate sharedposts"""
+    for sp in SharedPost.objects.all():
+        splits = generate_splits(sp)
+        if len(splits) > 1:
+            print [len(split) for split in splits]
+        # uncomment the next line to actually make the change
+        # update_splits(sp, groups)
+
+def generate_splits(sp):        
+    sp_receivers = SharedPostReceiver.objects \
+                   .filter(shared_post = sp) \
+                   .order_by('time')
+
+    if sp_receivers.count() == 0:
+        return []
+
+    splits = [ set() ] # useful for debugging output
+    current_shared_post = sp    
+    earliest_time = earliest_share_time(sp)
+            
+    for spr in sp_receivers:
+        if spr.time - earliest_time >= split_delta:
+            print 'needs to be split! ' + str(spr.shared_post.id) \
+                  + ':  ' + str(earliest_time) + '  ...  ' + str(spr.time)
+
+            # start a new set of posts
+            splits.append( set() )
+            earliest_time = spr.time
+
+        splits[-1].add(spr)
+
+    return splits
+
+def earliest_share_time(shared_post):
+    sprs = SharedPostReceiver.objects.filter(shared_post = shared_post) \
+           .order_by('time')
+
+    return sprs[0].time
+
+def update_splits(shared_post, splits):
+    """updates the shared post receivers to a new shared post if necessary"""
+    for i, split in enumerate(splits):
+        if i > 0: # the first group (index 0) can keep the original post
+            sp_copy = copy_shared_post(shared_post)
+            sp_copy.save()
+            
+            for spr in split:
+                spr.shared_post = sp_copy
+                spr.save()
+        
+
+def copy_shared_post(shared_post):
+    new_shared_post = SharedPost( \
+        post = current_shared_post.post,
+        sharer = current_shared_post.sharer,
+        comment = current_shared_post.comment,
+        bookmarklet = current_shared_post.bookmarklet,
+        thanks = current_shared_post.thanks,
+        clickthroughs = current_shared_post.clickthroughs)
+    return new_shared_post   
+    
+
+if __name__ == '__main__':
+    split_sharedposts()
