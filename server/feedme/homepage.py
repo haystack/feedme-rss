d2785f7cf62f251416e7a5d252813e2fe6d73beb
esperteria <esperteria@64f826ee-31e5-11de-94d6-134fa40061eb>
2009-10-25 00:54:34 +0000

    touched up homepage and reworded parts of tutorial

---
+++ server/feedme/homepage.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render_to_response
+from models import *
+import codecs, sys
+import versionutils
+
+# set stdout to Unicode so we can write Unicode strings to stdout
+# todo: create some sort of startup script which calls this
+sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+
+def homepage(request):
+    return render_to_response('index.html',
+                            {'feedme_url' : versionutils.latest_url()})
