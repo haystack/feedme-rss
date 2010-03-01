98d5aebeb5616a7aa9a4e43fad3a38f7dd06b0f3
esperteria <esperteria@64f826ee-31e5-11de-94d6-134fa40061eb>
2010-03-01 16:12:22 +0000

    patching /clickthrough/ to return 404 when the sharedpost does not exist

--- server/feedme/clickthrough.py
+++ server/feedme/clickthrough.py
@@ -1,13 +1,17 @@
 from models import *
 import codecs, sys
 from django.http import HttpResponseRedirect
+from django.http import Http404
 
 # set stdout to Unicode so we can write Unicode strings to stdout
 # todo: create some sort of startup script which calls this
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
 def clickthrough(request, sharedpost_pk):
-    shared_post = SharedPost.objects.get(pk = sharedpost_pk)
+    try:
+        shared_post = SharedPost.objects.get(pk = sharedpost_pk)
+    except SharedPost.DoesNotExist:
+        raise Http404
     shared_post.clickthroughs += 1
     shared_post.save()
 
