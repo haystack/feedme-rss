e87acdb17ec6745c1ab10e48e9689f60ce7f0030
marcua <marcua@64f826ee-31e5-11de-94d6-134fa40061eb>
2010-04-10 03:12:15 +0000

    check logged in jsonp endpoint

--- trunk/server/feedme/check_logged_in.py
+++ trunk/server/feedme/check_logged_in.py
@@ -3,7 +3,17 @@ from django.utils import simplejson
 from django.contrib.auth.models import User 
 from models import *
 
+
+def check_logged_in_jsonp(request):
+  response = check_logged_in_impl(request)
+  response = "%s(%s);" % (request.REQUEST['callback'], response)
+  return HttpResponse(response, \
+                      mimetype='application/json')
+
 def check_logged_in(request):
+    return HttpResponse(check_logged_in_impl(request), mimetype='application/json')
+
+def check_logged_in_impl(request):
     response = dict()
     response['logged_in'] = request.user.is_authenticated()
     response['user_interface'] = True
@@ -28,4 +38,4 @@ def check_logged_in(request):
             print 'not a study participant'
             
     response_json = simplejson.dumps(response)
-    return HttpResponse(response_json, mimetype='application/json')
+    return response_json
